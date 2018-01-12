0f8b15ac31348fdd2adfda5cb55869583e418240
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-01-13 00:55:25 +0200

    [image_preprocessing] added stub

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
+from image_preprocessing import image_preprocessing_page
+
+# app init
 app = Flask(__name__)
 
+# register blue prints
+app.register_blueprint(image_preprocessing_page, url_prefix="/preimg")
 
+# default route
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
