314c4385439a7e0b2c5deb7ae452e76c8af83870
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-01-13 12:32:17 +0200

    [image] refactored

--- image_preprocessing/Image.py
+++ image_preprocessing/Image.py
@@ -1,12 +1,12 @@
-from skimage import io, color
+from skimage import io
 
 
 class Image:
     def __init__(self, url):
         self._image = io.imread(url)
         print(self._image)
-        dimensions = color.guess_spatial_dimensions(self._image)
-        print(dimensions)
+        # dimensions = color.guess_spatial_dimensions(self._image)
+        # print(dimensions)
 
     def get_image(self):
         return self._image
