be8810f302de727df5a2c2899b8d48776d2d434f
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-01-13 15:02:43 +0200

    [Image] added Image.get_params() to view information about image

--- image_preprocessing/view.py
+++ image_preprocessing/view.py
@@ -4,6 +4,7 @@ from jinja2 import TemplateNotFound
 from .Image import Image
 
 DEFAULT_TEMPLATE = 'image_process'
+# TODO : remove absolute link
 TEST_IMAGE_URL = 'image_preprocessing/images/test.jpg'
 
 image_preprocessing_page = Blueprint('image_preprocessing_page', __name__, template_folder='templates')
@@ -13,6 +14,6 @@ image = Image(TEST_IMAGE_URL)
 @image_preprocessing_page.route('/', defaults={'page': DEFAULT_TEMPLATE})
 def show(page):
     try:
-        return render_template(page + '.html', data=image.get_image())
+        return render_template(page + '.html', data=image.get_params())
     except TemplateNotFound:
         abort(404)
