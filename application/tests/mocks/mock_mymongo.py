dea5001be9d2e050a4b30447d1245e4603ad5387
Vlad Taraman <knuckvlad@gmail.com>
2018-01-27 15:17:40 +0200

    fixes errors in unittests (mymongo)

--- application/tests/mocks/mock_mymongo.py
+++ application/tests/mocks/mock_mymongo.py
@@ -11,8 +11,9 @@ def get_all_images_count():
     return 4
 
 def get_image_by_hash(hash_string):
-    if hash_string == "820002000200a800":
-        return {'width': 1200, 'height': 1200, 'format': 'PNG', 'colors_count': 2, 'mono': True,
-                'hash': '820002000200a800'}
+    print(hash_string)
+    if str(hash_string) == "820002000200a800":
+        return bool({'width': 1200, 'height': 1200, 'format': 'PNG', 'colors_count': 2, 'mono': True,
+                'hash': '820002000200a800'})
     else:
-        return {}
+        return bool({})
