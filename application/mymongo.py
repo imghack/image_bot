2ab7e85a5aa92f9b1a41d8bfa124d84555ba3975
Maxim Romaniv <Maxim.Romaniv@netent.com>
2018-01-28 05:11:30 +0200

    [export] added basic logic

--- application/mymongo.py
+++ application/mymongo.py
@@ -1,5 +1,7 @@
 import pymongo
+import os
 import settings
+import xml.etree.cElementTree as ET
 
 connection = pymongo.MongoClient(settings.HOST, settings.PORT)
 db = connection[settings.DB]
@@ -24,6 +26,25 @@ def get_all_images_count():
     return cursor.count()
 
 
+def export_to_xml():
+    """
+    Export to all images to xml file
+    """
+    root = ET.Element("root")
+    images = ET.SubElement(root, "images")
+
+    all_images = cursor.find({})
+    for document in all_images:
+        sub_element = ET.SubElement(images, "image")
+        sub_element.text = document['url']
+        for key in document:
+            sub_element.set(key, str(document[key]))
+
+    tree = ET.ElementTree(root)
+    tree.write(os.path.join(settings.DOWNLOADS_FOLDER, settings.DOWNLOAD_IMAGE_XML_FILE_NAME),
+               **settings.XML_PROPERTIES)
+
+
 def _get_image_by_hash(hash_string):
     """Private Method used to get image tuple by hash
     :param hash_string: the p-hash string
@@ -32,3 +53,5 @@ def _get_image_by_hash(hash_string):
     col = cursor.find({'hash': hash_string})
     return col[0] if col.count() > 0 else {}  # if there are values in db return the first one else empty({})
 
+if __name__ == '__main__':
+    export_to_xml()
--- settings.py
+++ settings.py
@@ -4,3 +4,11 @@ DB = 'test'
 COLLECTION = 'image'
 HOST = 'localhost'
 PORT = 27017
+
+XML_PROPERTIES = {
+    'xml_declaration': True,
+    'method': 'xml',
+    'encoding': 'UTF-8'
+}
+DOWNLOADS_FOLDER = 'download/'
+DOWNLOAD_IMAGE_XML_FILE_NAME = 'images.xml'
